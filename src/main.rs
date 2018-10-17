4ab518e0616926d728504c61019b4da581b150c1
Jasper Bekkers <bekkers@gmail.com>
2018-10-17 15:10:25 +0200

    composite rejection

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ extern crate rand;
 use rand::*;
 
 struct Outcome {
-    payload: usize,
+    light_idx: usize,
     idx: usize,
     rate: f32,
 }
 
 struct RejectionMethod {
-    last_inserted: usize,
     max_rate: f32,
     outcomes: Vec<Outcome>,
 }
@@ -17,20 +16,19 @@ struct RejectionMethod {
 impl RejectionMethod {
     fn new(max_rate: f32) -> Self {
         Self {
-            last_inserted: 0,
             max_rate: max_rate,
             outcomes: vec![],
         }
     }
 
-    fn add(&mut self, rate: f32, payload: usize) {
+    fn add(&mut self, rate: f32, light_idx: usize) {
         if rate > self.max_rate {
             // todo: clamp rate, or something
             panic!("Invalid rate");
         }
 
         let outcome = Outcome {
-            payload: payload,
+            light_idx: light_idx,
             rate: rate,
             idx: self.outcomes.len(),
         };
@@ -40,15 +38,17 @@ impl RejectionMethod {
 
     fn delete(&mut self, outcome_idx: usize) {
         self.outcomes.swap_remove(outcome_idx);
+        if self.outcomes.len() != 0 {
+            self.outcomes[outcome_idx].idx = outcome_idx;
+        }
     }
 
     fn update(&mut self, outcome_idx: usize, rate: f32) {
-        if rate == 0.0 && self.outcomes[outcome_idx].rate > 0.0 {
-            // self.delete(outcome_idx);
-            self.outcomes.swap_remove(outcome_idx);
-        } else if (rate > 0.0 && self.outcomes[outcome_idx].rate == 0.0) {
-            //  self.add(outcome_idx);
-        }
+        // if rate == 0.0 && self.outcomes[outcome_idx].rate > 0.0 {
+        //     self.delete(outcome_idx);
+        // } else if (rate > 0.0 && self.outcomes[outcome_idx].rate == 0.0) {
+
+        // }
         let mut outcome = &mut self.outcomes[outcome_idx];
 
         outcome.rate = rate;
@@ -58,16 +58,59 @@ impl RejectionMethod {
         let mut loop_count = 0;
         loop {
             loop_count += 1;
-            let k = rng.gen_range::<f32>(0.0, self.outcomes.len() as f32);
-            let rand_idx = k.floor();
-            let rand_rate = (k - rand_idx) * self.max_rate;
+            let rand = rng.gen_range::<f32>(0.0, self.outcomes.len() as f32);
+            let rand_idx = rand.floor();
+            let rand_rate = (rand - rand_idx) * self.max_rate;
 
             let outcome = &self.outcomes[rand_idx as usize];
 
             if outcome.rate >= rand_rate {
-                return (loop_count, outcome.payload);
+                return (loop_count, outcome.idx);
             }
         }
+    }
+}
+
+struct CompositeRejectionMethod {
+    groups: Vec<RejectionMethod>,
+    sum_rates: Vec<f32>,
+    total_rate: f32,
+    constant: f32,
+    max: f32,
+}
+
+impl CompositeRejectionMethod {
+    fn new(num_groups: usize, max: f32) -> Self {
+        Self {
+            groups: vec![],
+            sum_rates: vec![],
+            total_rate: 0.0,
+            constant: 0.0,
+            max: max,
+        }
+    }
+
+    fn add(&mut self, rate: f32, light_idx: usize) {
+        let group_idx = (self.max / rate).log(self.constant).floor() as usize;
+        self.groups[group_idx].add(rate, light_idx);
+        self.sum_rates[group_idx] += rate;
+        self.total_rate += rate;
+    }
+
+    fn delete(&mut self, outcome_idx: usize) {}
+
+    fn extract<T: Rng>(&mut self, rng: &mut T) -> (u32, usize) {
+        let u = rng.gen::<f32>();
+        let mut rand = u * self.total_rate;
+        for (idx, g) in self.groups.iter_mut().enumerate() {
+            if self.sum_rates[idx] >= rand {
+                return g.extract(rng);
+            }
+
+            rand = rand - self.sum_rates[idx];
+        }
+
+        panic!("Shouldn't be able to reach here, algorithm invariant breached");
     }
 }
 
